7e98d841f47af9b0e2d6fcf9e7c77c8a8867f023
Mobilszoftver laboratórium <mobsoft@aut.bme.hu>
2016-04-11 08:41:59 +0200

    Initial commit

---
+++ app/src/main/java/n358vu/krisa/youtubemostpopular/MainActivity.java
@@ -0,0 +1,13 @@
+package n358vu.krisa.youtubemostpopular;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
